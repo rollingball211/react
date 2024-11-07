06c05544c4d5a742e43cb46ba0d167f35b390376
jinhyeon <jingoo211@gmail.com>
2024-11-07 21:15:54 +0900

    state

---
+++ my-app/src/chapter_04/props_ex1.jsx
@@ -0,0 +1,11 @@
+// import React from "react";
+
+// function App(props) {
+//     return (
+//         <Profile 
+//             name = "지년"
+//             introdunction = "안녕하세요 저입니다"
+//             viewCount = {1500}
+//         /> 
+//     );
+// }
---
+++ my-app/src/chapter_06/Notification.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const styles = {
+    wrapper: {
+        margin : 8,
+        padding : 8,
+        display : "flex",
+        flexDirection : "row",
+        border : "1px solid grey",
+        boderRadius : 16,
+    },
+
+    messageText : {
+        color : "black",
+        fontSize : 16,
+    },
+};
+
+class Notification extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {}; //state 에 아무런 데이터도 가지고 있지 않음
+    }
+
+    render() {
+        return (
+            <div style= {styles.wrapper}>
+                <span style ={styles.messageText}>{this.props.message}</span>
+            </div>
+      
+        );
+    }
+}
+
+export default Notification;
