5e9c439f250370bdd9b5a214d3e32a720d909fcd
jinhyeon <jingoo211@gmail.com>
2024-12-03 23:26:52 +0900

    useMemo_1

---
+++ my-app/src/chapter_07/useMemo.jsx
@@ -0,0 +1,8 @@
+import React,{ useMemo } from "react";
+
+// const memoizedValue = useMemo(
+//     () => {
+//         return computeExpensiveValue (의존성 변수1, 의존성 변수2);
+//     },
+//     [의존성 변수1, 의존성 변수2]
+// );
