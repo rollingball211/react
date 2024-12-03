d263807c1b74c24c0d130ae2e8c46f617f77ef55
jinhyeon <jingoo211@gmail.com>
2024-12-03 23:21:50 +0900

    memoization

--- my-app/src/chapter_07/ex1.jsx
+++ my-app/src/chapter_07/ex1.jsx
@@ -5,13 +5,10 @@ function Counter(props) {
 
     return (
     <div>
-        <p> 총 {count} / {count+1} 번 클릭했습니다.</p>
+        <p> 총 {count} 번 클릭했습니다.</p>
         <button onClick={ () => setCount(count + 1 )}>
             클릭
         </button>
     </div>
 )
 }
-
-
-
